0d1facada781ecd555d087af3346e3667574e372
Matthew McKenzie <mwmckenzie@gmail.com>
2024-07-01 18:58:49 +0100

    Implement data scraping for Roman villas
    In the main program, the data scraping is performed from a given Wikipedia page using the newly implemented VillaDataScraper class. This class uses HtmlAgilityPack for HTML parsing, and includes methods for fetching and extracting villa data, along with saving this data into a JSON file. Additionally, new package references are added to the project file.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using RomanVillaDataScraper;
+
+var url = "https://en.wikipedia.org/wiki/List_of_Roman_villas_in_England";
+
+var ds = new VillaDataScraper(new HttpClient());
+var list = await ds.FetchVillaInfo(url);
+
+var json = System.Text.Json.JsonSerializer.Serialize(list);
+await File.WriteAllTextAsync("villas.json", json);
--- VillaDataScraper.cs
+++ VillaDataScraper.cs
@@ -15,9 +15,82 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+using RomanVillaLibrary.Models;
+
 namespace RomanVillaDataScraper;
 
-public class VillaDataScraper
+public partial class VillaDataScraper(HttpClient httpClient)
 {
-    
+    public async Task<List<VillaInfo>> FetchVillaInfo(string url)
+    {
+        Console.Write($"FetchVillaInfo URL: {url}");
+        var data = new List<VillaInfo>();
+        
+        var response = await httpClient.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+        var pageContents = await response.Content.ReadAsStringAsync();
+
+        var document = new HtmlDocument();
+        document.LoadHtml(pageContents);
+
+        var tables = document.DocumentNode
+            .SelectNodes("//div[@id='mw-content-text']//table[contains(@class, 'wikitable')]");
+
+        if (tables == null) return data;
+        
+        foreach (var table in tables)
+        {
+            var rows = table.SelectNodes(".//tr[position()>1]"); // Skip the header row
+
+            if (rows != null)
+            {
+                ExtractDataFromRows(rows, data);
+            }
+        }
+
+        return data;
+    }
+
+    private void ExtractDataFromRows(HtmlNodeCollection rows, List<VillaInfo> data)
+    {
+        foreach (var row in rows)
+        {
+            var cells = row.SelectNodes("td");
+
+            if (cells is { Count: >= 4 })
+            {
+                CreateVillaInfo(cells, data);
+            }
+        }
+    }
+
+    private void CreateVillaInfo(HtmlNodeCollection cells, List<VillaInfo> data)
+    {
+        var villaName = MyRegex().Replace(cells[0].InnerText.Trim(), "");
+
+        var wikiLinkText = cells[1].InnerText.Trim();
+        var wikiLink = cells[1].SelectSingleNode(".//a")?.GetAttributeValue("href", "N/A") ?? "N/A";
+
+        var gridReference = cells[2].InnerText.Trim();
+
+        var link = cells[3].SelectSingleNode(".//a")?.GetAttributeValue("href", "N/A") ?? "N/A";
+
+        // Clean up text
+        wikiLink = wikiLink.Replace(",", "").Replace("\"", "");
+        wikiLinkText = wikiLinkText.Replace(",", "").Replace("\"", "");
+
+        data.Add(new VillaInfo
+        {
+            VillaName = villaName,
+            WikiLinkText = wikiLinkText,
+            WikiLink = wikiLink,
+            GridReference = gridReference,
+            ExternalLink = link
+        });
+    }
+
+    [GeneratedRegex(@"[^a-zA-Z]")]
+    private static partial Regex MyRegex();
 }
